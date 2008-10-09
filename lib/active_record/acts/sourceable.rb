03e856efac4089d24054aaedbdeae3f9472ca5c4
Nicholas Jakobsen <nicholas.jakobsen@telus.net>
2008-10-09 08:53:58 +0000

    Mediator seems to be working for item types and notes and sourceable_sites. Modified item table to add datestamp Fixed bug in sourceable_site migration where sourceable_type was an integer instead of a string
    git-svn-id: http://rrnpilot.svnrepository.com/svn/rrn/mediator/data_mapper/trunk/vendor/plugins/acts_as_sourceable@2386 7a3a9612-1c34-0410-bb05-cf807c794e1a

--- lib/active_record/acts/sourceable.rb
+++ lib/active_record/acts/sourceable.rb
@@ -9,7 +9,6 @@ module ActiveRecord
         def acts_as_sourceable(options = {})
           
           class_eval <<-EOV
-
             include ActiveRecord::Acts::Sourceable::InstanceMethods
 
             has_many :sourceable_sites, :as => :sourceable
@@ -23,9 +22,9 @@ module ActiveRecord
       module InstanceMethods
         private
         def record_source
-          raise 'acts_as_sourceable cannot save because no global variable $SITE has been set for this conversion session.' unless $SITE
-          sourceable_site = SourceableSite.find_or_inititalize_by_source_id_and_sourceable_id_and_sourceable_type(:sourceable_type => self.class, :sourceable_id => id, :source_id => $SITE.id)
-          sourceable_site.source = $SITE
+          raise 'acts_as_sourceable cannot save because no global variable $SITE has been set for this conversion session.' if $SITE.nil?
+          sourceable_site = SourceableSite.new
+          sourceable_site.site = $SITE
           sourceable_site.sourceable = self
           sourceable_site.save!
         end
