a391bb5ed4d71ca4cbc33c6b31acaaa1238442be
Nicholas Jakobsen <nicholas.jakobsen@telus.net>
2009-03-23 20:17:31 +0000

    Updating sourceable so you can add a condition for destruction of the instance of each type of model during garbage collection

--- lib/active_record/acts/sourceable.rb
+++ lib/active_record/acts/sourceable.rb
@@ -1,25 +1,28 @@
 module ActiveRecord
   module Acts #:nodoc:
     module Sourceable #:nodoc:
-      def self.included(base)
-        base.extend(ClassMethods)
+      def acts_as_sourceable(options = {})
+        # Include Class and Instance Methods
+        extend ClassMethods unless (class << self; included_modules; end).include?(ClassMethods)
+        include InstanceMethods unless included_modules.include?(InstanceMethods)
+
+        has_many :sourceable_sites, :as => :sourceable
+        has_many :sources, :through => :sourceable_sites, :source => :site
+
+        after_save :record_source
+        before_destroy :destroy_condition
+
+        cattr_accessor :_acts_as_sourceable_options
+
+        # Keep track of the options we passed
+        options.assert_valid_keys :condition
+        self._acts_as_sourceable_options = options
+
+        # Keep a list of all classes that are sourceable
+        SourceableSite.sourceable_classes << self
       end
 
       module ClassMethods
-        def acts_as_sourceable(options = {})
-          
-          class_eval <<-EOV
-            include ActiveRecord::Acts::Sourceable::InstanceMethods
-
-            has_many :sourceable_sites, :as => :sourceable
-            has_many :sites, :through => :sourceable_sites
-
-            after_save :record_source
-
-            SourceableSite.sourceable_classes << self
-          EOV
-        end
-        
         def garbage_collect
           # Destroy all entries of this class which no longer have a SourceableSite
           destroy_all("NOT EXISTS (SELECT * FROM sourceable_sites WHERE sourceable_sites.sourceable_type = '#{class_name}' AND sourceable_sites.sourceable_id = #{table_name}.id)")
@@ -27,8 +30,22 @@ module ActiveRecord
       end
 
       module InstanceMethods
-        
+
         private
+
+        # Called on destroy to determine whether or not the record should be destroyed
+        # Allows models to add a condition for destruction that can prevent the model from being garbage collected
+        def destroy_condition
+          case self.class._acts_as_ordered_options[:condition]
+          when Proc
+            return self.class._acts_as_ordered_options[:condition].call
+          when nil
+            return true
+          else
+            return self.class._acts_as_ordered_options[:condition]
+          end
+        end
+
         def record_source
           if SourceableSite.record
             raise 'acts_as_sourceable cannot save because no global variable $SITE has been set for this conversion session.' if $SITE.nil?
