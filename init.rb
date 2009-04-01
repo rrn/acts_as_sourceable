f1561150b53f0fb7c42698504974d62f6862a66f
Tristan Goffman <goffmant@interchange.ubc.ca>
2009-04-01 02:37:47 +0000

    Acts/AbstractPermissable, Acts/Permissable, Acts/PermissableProxy and spec updates, all specs passing. Including most recent Everyone overriding specific User spec

--- init.rb
+++ init.rb
@@ -3,3 +3,4 @@ require 'active_record'
 require 'active_record/acts/sourceable'
 require 'active_record/acts/sourceable_site'
 ActiveRecord::Base.send(:extend, ActiveRecord::Acts::Sourceable::ActMethod)
+  
