ef4ef8914e0918b6c717c5bafab092482e3390ef
Ryan Wallace <rywall@gmail.com>
2009-06-17 09:44:45 -0700

    renamed site to holding institution

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 $:.unshift "#{File.dirname(__FILE__)}/lib"
 require 'active_record'
 require 'active_record/acts/sourceable'
-require 'active_record/acts/sourceable_site'
+require 'active_record/acts/sourceable_institution'
 ActiveRecord::Base.send(:extend, ActiveRecord::Acts::Sourceable::ActMethod)
   
--- lib/active_record/acts/sourceable_institution.rb
+++ lib/active_record/acts/sourceable_institution.rb
@@ -1,14 +1,14 @@
-class SourceableSite < ActiveRecord::Base
+class SourceableInstitution < ActiveRecord::Base
   #Tristan: Ugly hack until someone takes the replaceable out of the sourceable, plugins are standalone, son!
   if defined?(RAILS_ENV)
-    acts_as_replaceable :conditions => [:sourceable_type, :sourceable_id, :site_id]
+    acts_as_replaceable :conditions => [:sourceable_type, :sourceable_id, :holding_institution_id]
   end
   belongs_to :sourceable, :polymorphic => true
-  belongs_to :site
+  belongs_to :holding_institution
 
-  validates_presence_of :sourceable_type, :sourceable_id, :site_id
+  validates_presence_of :sourceable_type, :sourceable_id, :holding_institution_id
   
-  @@record = true # enables or disables automatic creation of SourceableSites when a sourceable is saved
+  @@record = true # enables or disables automatic creation of SourceableInstitutions when a sourceable is saved
   @@sourceable_classes = Array.new
   cattr_reader :sourceable_classes
   cattr_accessor :record
