0ea04d8befbb76e0251b97926365b881ca33fdbf
Ryan Wallace <rywall@gmail.com>
2010-04-26 13:17:52 -0400

    Add orphaned derived categories to unsourced categories.

--- lib/active_record/acts/sourceable.rb
+++ lib/active_record/acts/sourceable.rb
@@ -11,7 +11,7 @@ module ActiveRecord
           has_many :sources, :through => :sourceable_institutions, :source => :holding_institution
 
           named_scope :sourced, {:select => "DISTINCT #{table_name}.*", :joins => :sourceable_institutions}
-          named_scope :unsourced, {:joins => "LEFT OUTER JOIN sourceable_institutions ON sourceable_institutions.sourceable_type = '#{class_name}' AND #{table_name}.id = sourceable_institutions.sourceable_id", :conditions => "sourceable_institutions.id IS NULL #{"AND #{table_name}.derived = false" if column_names.include?('derived')}"}
+          named_scope :unsourced, {:joins => "LEFT OUTER JOIN sourceable_institutions ON sourceable_institutions.sourceable_type = '#{class_name}' AND #{table_name}.id = sourceable_institutions.sourceable_id #{"LEFT OUTER JOIN flattened_item_#{table_name} ON #{table_name}.id = flattened_item_#{table_name}.#{table_name.singularize}_id" if column_names.include?('derived')}", :conditions => "sourceable_institutions.id IS NULL #{"AND #{table_name}.derived = false OR (#{table_name}.derived = true AND flattened_item_#{table_name}.id IS NULL)" if column_names.include?('derived')}"}
 
           after_save :record_source
 
